218cc8b1a8ce81ae4fb97ad5b9010323507d3dc1
JeroPizarro <pizarro.jero@gmail.com>
2024-03-21 23:57:39 +0100

    Section-11 added. Async JS. Fetch API.

---
+++ section-11/script.js
@@ -0,0 +1,79 @@
+(function () {
+  //Fetch API with std promise sintax. GET is the default HTTP verb.
+  fetch('../json/movies.json')
+    .then((response) => {
+      //You'll get first the response obj with info about the response
+      console.log(response);
+      return response.json(); //return data
+      //use response.text() to return data from txt file
+    })
+    .then((data) => {
+      console.log(data);
+    });
+
+  //Fetch from API
+  fetch('http://api.github.com/users')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+
+  const generateButton = document.querySelector('button');
+  const userContainer = document.querySelector('.user');
+  const userImage = userContainer.querySelector('#user-image');
+  const userInfo = userContainer.querySelector('#user-info');
+
+  const init = () => {
+    getRandomUser();
+  };
+
+  const getRandomUser = () => {
+    fetch('https://randomuser.me/api/')
+      .then((response) => response.json())
+      .then((data) => {
+        fillUserData(data);
+        changeBackground(data.results[0].gender);
+      });
+  };
+
+  const createUserData = (newRandomUser) => {
+    const userInfo = {
+      name: `${newRandomUser.name.first} ${newRandomUser.name.last}`,
+      age: newRandomUser.registered.age,
+      location: `${newRandomUser.location.street.name} ${newRandomUser.location.street.number}, ${newRandomUser.location.state}, ${newRandomUser.location.country}.`,
+      phone: newRandomUser.phone,
+      email: newRandomUser.email,
+    };
+
+    return userInfo;
+  };
+
+  const fillUserData = (data) => {
+    const newRandomUser = data.results[0];
+    const userData = createUserData(newRandomUser);
+
+    userImage.src = newRandomUser.picture.large;
+
+    userInfo.innerHTML = '';
+    Object.keys(userData).forEach((key) => {
+      const li = document.createElement('li');
+      const capitalizeKey = `${key[0].toLocaleUpperCase() + key.slice(1)}`;
+
+      li.innerText = `${capitalizeKey}: ${userData[key]}`;
+
+      userInfo.appendChild(li);
+    });
+  };
+
+  const changeBackground = (gender) => {
+    console.log(gender);
+    if (gender === 'male') {
+      document.body.classList.add('background-blue');
+    } else {
+      document.body.classList.remove('background-blue');
+    }
+  };
+
+  generateButton.addEventListener('click', getRandomUser);
+  addEventListener('DOMContentLoaded', init);
+})();
