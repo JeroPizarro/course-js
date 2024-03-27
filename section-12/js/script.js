b49f4575505c0b45470954d1188df1fa96b65a22
JeroPizarro <pizarro.jero@gmail.com>
2024-03-28 00:18:20 +0100

    Section-12 added. Flixx project. Popular shows and movie details.

--- section-12/js/script.js
+++ section-12/js/script.js
@@ -49,17 +49,142 @@
       `;
       document.getElementById('popular-movies').appendChild(div);
     });
-    console.log(results);
+  };
+
+  const displayPopularShows = async () => {
+    const { results } = await fetchData('tv/popular', 'GET');
+
+    results.forEach((show) => {
+      const div = document.createElement('div');
+      div.setAttribute('class', 'card');
+      div.innerHTML = `
+        <a href="tv-details.html?${show.id}">
+          ${
+            show.poster_path
+              ? `<img
+            src="https://image.tmdb.org/t/p/w500/${show.poster_path}"
+            class="card-img-top"
+            alt="${show.name} poster"
+          />`
+              : `<img
+            src="images/no-image.jpg"
+            class="card-img-top"
+            alt="${show.name} poster"
+          />`
+          }
+        </a>
+        <div class="card-body">
+          <h5 class="card-title">${show.name}</h5>
+          <p class="card-text">
+            <small class="text-muted">Air Date: ${show.first_air_date}</small>
+          </p>
+        </div>
+      `;
+      document.getElementById('popular-shows').appendChild(div);
+    });
+  };
+
+  const displayMovieDetails = async () => {
+    const movieId = window.location.search.slice(1);
+    const movieDetails = await fetchData(`movie/${movieId}`, 'GET');
+
+    displayBackdropImage('movie', movieDetails.backdrop_path);
+
+    const div = document.createElement('div');
+    const template = `
+      <div class="details-top">
+        <div>
+          ${
+            movieDetails.poster_path
+              ? `<img
+            src="https://image.tmdb.org/t/p/w500/${movieDetails.poster_path}"
+            class="card-img-top"
+            alt="${movieDetails.name} poster"
+          />`
+              : `<img
+            src="images/no-image.jpg"
+            class="card-img-top"
+            alt="${movieDetails.name} poster"
+          />`
+          }
+        </div>
+        <div>
+          <h2>${movieDetails.title}</h2>
+          <p>
+            <i class="fas fa-star text-primary"></i>
+            ${movieDetails.vote_average.toFixed(1)} / 10
+          </p>
+          <p class="text-muted">Release Date: ${movieDetails.release_date}</p>
+          <p>
+            ${movieDetails.overview}
+          </p>
+          <h5>Genres</h5>
+          <ul class="list-group">
+            ${movieDetails.genres
+              .map((genre) => `<li>${genre.name}</li>`)
+              .join('')}
+          </ul>
+          <a href="${
+            movieDetails.homepage
+          }" target="_blank" class="btn">Visit Movie Homepage</a>
+        </div>
+      </div>
+      <div class="details-bottom">
+        <h2>Movie Info</h2>
+        <ul>
+          <li><span class="text-secondary">Budget:</span> $${addCommasToNumbers(
+            movieDetails.budget
+          )}</li>
+          <li><span class="text-secondary">Revenue:</span> $${addCommasToNumbers(
+            movieDetails.revenue
+          )}</li>
+          <li><span class="text-secondary">Runtime:</span> ${
+            movieDetails.runtime
+          }</li>
+          <li><span class="text-secondary">Status:</span> ${
+            movieDetails.status
+          }</li>
+        </ul>
+        <h4>Production Companies</h4>
+        <div class="list-group">${movieDetails.production_companies
+          .map((company) => `<span>${company.name}</span>`)
+          .join(', ')}</div>
+      </div>`;
+    div.innerHTML = template;
+    document.querySelector('#movie-details').append(div);
+    console.log(movieDetails);
+  };
+
+  const displayBackdropImage = (type, backdropPath) => {
+    //.backdrop-container
+    const overlayDiv = document.createElement('div');
+    overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backdropPath})`;
+    console.log(overlayDiv.style.backgroundImage);
+    overlayDiv.classList.add('backdrop-container');
+
+    if (type === 'movie') {
+      document.querySelector('#movie-details').appendChild(overlayDiv);
+    } else {
+      document.querySelector('#show-details').appendChild(overlayDiv);
+    }
   };
 
   const fetchData = async (endpoint, httpVerb) => {
+    toggleSpinner();
+
     const API_KEY = '4e5d6b9c93a4351d3a6125dc38e3a5a6';
     const API_URL = 'https://api.themoviedb.org/3/';
     const res = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}`, {
       method: `${httpVerb}`,
     });
     const data = await res.json();
+
+    toggleSpinner();
     return data;
+  };
+
+  const toggleSpinner = () => {
+    document.querySelector('.spinner').classList.toggle('show');
   };
 
   const highlightActiveLink = () => {
@@ -72,17 +197,20 @@
     });
   };
 
+  const addCommasToNumbers = (number) => {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  };
+
   const init = () => {
     //Router - exec diffetent js in each page
     switch (global.currentPage) {
       case global.basePath:
       case '/index.html':
-        console.log('basepath');
         displayPopularMovies();
         break;
 
       case global.showsPath:
-        console.log('shows');
+        displayPopularShows();
         break;
 
       case global.showDetailPath:
@@ -90,7 +218,7 @@
         break;
 
       case global.movieDetailPath:
-        console.log('movie-det');
+        displayMovieDetails();
         break;
 
       case global.searchPath:
